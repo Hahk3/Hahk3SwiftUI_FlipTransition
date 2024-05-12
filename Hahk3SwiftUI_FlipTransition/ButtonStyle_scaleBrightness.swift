94b38c5ba3983e3a584bff033de13a9ca0f36800
Hahk3 <timelesstm@naver.com>
2024-05-12 19:27:03 +0900

    1

---
+++ Hahk3SwiftUI_FlipTransition/ButtonStyle_scaleBrightness.swift
@@ -0,0 +1,18 @@
+//
+//  ButtonStyle_scaleBrightness.swift
+//  Hahk3SwiftUI_FlipTransition
+//
+//  Created by Hakjin Kim on 5/12/24.
+//
+
+import SwiftUI
+
+struct ButtonStyle_scaleBrightness: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ButtonStyle_scaleBrightness()
+}
---
+++ Hahk3SwiftUI_FlipTransition/FlipTransition.swift
@@ -0,0 +1,18 @@
+//
+//  FlipTransition.swift
+//  Hahk3SwiftUI_FlipTransition
+//
+//  Created by Hakjin Kim on 5/12/24.
+//
+
+import SwiftUI
+
+struct FlipTransition: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FlipTransition()
+}
