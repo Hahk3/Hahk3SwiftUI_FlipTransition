61f9217969059b03acb5a849fac2d9541aa68695
Hahk3 <timelesstm@naver.com>
2024-05-12 16:26:59 +0900

    Initial Commit

---
+++ Hahk3SwiftUI_FlipTransition/Hahk3SwiftUI_FlipTransitionApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hahk3SwiftUI_FlipTransitionApp.swift
+//  Hahk3SwiftUI_FlipTransition
+//
+//  Created by Hakjin Kim on 5/12/24.
+//
+
+import SwiftUI
+
+@main
+struct Hahk3SwiftUI_FlipTransitionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
